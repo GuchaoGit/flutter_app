d2b7c27781fe8394080964cffde02e2fb963dd3f
guchaochao <guchaochao@huiyunit.com>
2019-05-24 09:09:40 +0800

    还原main方法

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/WebView.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
